f5b25748f8192384006c074f7af51a5025ac74b4
Trever Shick <trevershick@gmail.com>
2013-01-28 20:52:15 -0500

    Annotations are not top level any more

--- src/main/java/com/github/trevershick/test/ldap/annotations/LdapEntry.java
+++ src/main/java/com/github/trevershick/test/ldap/annotations/LdapEntry.java
@@ -1,11 +1,9 @@
 package com.github.trevershick.test.ldap.annotations;
 
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 
-@Target(ElementType.TYPE)
+
 @Retention(RetentionPolicy.RUNTIME)
 public abstract @interface LdapEntry {
 	String dn();
