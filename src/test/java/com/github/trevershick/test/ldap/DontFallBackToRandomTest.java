0ea82869e0eb7abbce7734dc125edfaadf29ff68
Trever Shick <trevershick@gmail.com>
2013-01-28 21:06:03 -0500

    ensure ldap servers shutdown at the end of unit tests

--- src/test/java/com/github/trevershick/test/ldap/DontFallBackToRandomTest.java
+++ src/test/java/com/github/trevershick/test/ldap/DontFallBackToRandomTest.java
@@ -1,18 +1,26 @@
 package com.github.trevershick.test.ldap;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.net.BindException;
 
+import org.junit.After;
 import org.junit.Test;
 
 import com.github.trevershick.test.ldap.annotations.LdapConfiguration;
 
 public class DontFallBackToRandomTest {
 	
+	private LdapServerResource s1;
+	
+	@After
+	public void cleanup() {
+		s1.stop();
+	}
+	
 	@Test(expected=BindException.class)
 	public void wontFallbackToRandom() throws Exception {
-		LdapServerResource s1 = new LdapServerResource().start();
+		s1 = new LdapServerResource().start();
 		assertEquals(LdapConfiguration.DEFAULT_PORT, s1.port());
 		
 		new LdapServerResource().start();
--- src/test/java/com/github/trevershick/test/ldap/FallBackToRandomTest.java
+++ src/test/java/com/github/trevershick/test/ldap/FallBackToRandomTest.java
@@ -3,6 +3,7 @@ package com.github.trevershick.test.ldap;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.After;
 import org.junit.Test;
 
 import com.github.trevershick.test.ldap.annotations.LdapConfiguration;
@@ -10,12 +11,24 @@ import com.github.trevershick.test.ldap.annotations.LdapConfiguration;
 @LdapConfiguration(useRandomPortAsFallback=true)
 public class FallBackToRandomTest {
 	
+	private LdapServerResource s1;
+	private LdapServerResource s2;
+
+	@After
+	public void cleanup1() {
+		s1.stop();
+	}
+	@After
+	public void cleanup2() {
+		s2.stop();
+	}
+
 	@Test
 	public void wontFallbackToRandom() throws Exception {
-		LdapServerResource s1 = new LdapServerResource().start();
+		s1 = new LdapServerResource().start();
 		assertEquals(LdapConfiguration.DEFAULT_PORT, s1.port());
 		
-		LdapServerResource s2 = new LdapServerResource(this).start();
+		s2 = new LdapServerResource(this).start();
 		assertTrue(s2.isStarted());
 		assertTrue(s2.port() != LdapConfiguration.DEFAULT_PORT);
 		
