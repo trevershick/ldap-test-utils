dd2fd95770efd186502bfd82bd0b5844b6b81cca
Trever Shick <trevershick@gmail.com>
2013-01-28 20:51:47 -0500

    @LdapConfiguration should be inherited.

--- src/main/java/com/github/trevershick/test/ldap/annotations/LdapConfiguration.java
+++ src/main/java/com/github/trevershick/test/ldap/annotations/LdapConfiguration.java
@@ -1,10 +1,12 @@
 package com.github.trevershick.test.ldap.annotations;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Inherited
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface LdapConfiguration {
@@ -14,6 +16,7 @@ public @interface LdapConfiguration {
 	public static final String DEFAULT_PASSWORD = "password";
 	public static final String DEFAULT_BIND_DN = "cn=admin";
 	
+	boolean useRandomPortAsFallback() default false;
 	int port() default DEFAULT_PORT;
 	LdapEntry base() default @LdapEntry(objectclass=DEFAULT_ROOT_ENTRY_OBJECTCLASS,dn=DEFAULT_ROOT_OBJECT_DN);
 	LdapEntry[] entries() default {};
---
+++ src/test/java/com/github/trevershick/test/ldap/AnnotationsAreInheritedTest.java
@@ -0,0 +1,18 @@
+package com.github.trevershick.test.ldap;
+
+import org.junit.Test;
+
+public class AnnotationsAreInheritedTest extends CustomEntriesAnnotationsTest {
+
+	/**
+	 * This should inherit the custom configuration from CustomEntriesAnnotationTest.
+	 * Thus, it can call the parent class test and it should still pass.  I can't change
+	 * the inherited attribute of the annotation to false for a negative test, but i
+	 * did remove the @Inherited from the annotation and reran the test and this test
+	 * then fails.
+	 */  
+	@Test
+	public void shouldInheritConfigFromParentClass() throws Exception {
+		super.testStartsUpWithMyEntries();
+	}
+}
